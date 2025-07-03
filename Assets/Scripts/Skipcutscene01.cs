707a6ec1d6cbd3686265b6a3cf1537125a1ea4e7
Pannawit-10 <52687@yupparaj.ac.th>
2025-07-03 23:11:19 +0700

    update-3-7-25-3

---
+++ Assets/Scripts/Skipcutscene01.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoEndHandler : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public string nextSceneName;
+
+    void Start()
+    {
+        videoPlayer.loopPointReached += EndReached;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
+
+    void EndReached(VideoPlayer vp)
+    {
+        SceneManager.LoadScene(nextSceneName);
+    }
+}

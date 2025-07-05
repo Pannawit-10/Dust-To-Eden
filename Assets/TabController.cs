281aee1299f2eb2dd5c413eda778cc477836b5a3
poommynynano <52936@yupparaj.ac.th>
2025-07-05 17:46:56 +0700

    kuy_tabadd

--- Assets/TabController.cs
+++ Assets/TabController.cs
@@ -1,17 +1,19 @@
 using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 
-public class Tabscontroller : MonoBehaviour
+
+public class TabController : MonoBehaviour
 {
     public Image[] tabImages;
     public GameObject[] pages;
-    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //ActivateTab(0);
+        ActivateTab(0);
     }
 
+    // Update is called once per frame
     public void ActivateTab(int tabNo)
     {
         for (int i = 0; i < pages.Length; i++)

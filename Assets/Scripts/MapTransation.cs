d60891af70be76ee49351d6ed7151d45be61e3e1
poommynynano <52936@yupparaj.ac.th>
2025-07-07 21:51:11 +0700

    maptransitioncomplete

---
+++ Assets/Scripts/MapTransation.cs
@@ -0,0 +1,55 @@
+using Unity.Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MapTransation : MonoBehaviour
+{
+    [SerializeField] PolygonCollider2D mapBoundry;
+    CinemachineConfiner2D confiner;
+    [SerializeField] Direction direction;
+    [SerializeField] float additivePos = 2f;
+
+    enum Direction { Up, Down, Left, Right }
+
+    private void Awake()
+    {
+        confiner = Object.FindFirstObjectByType<CinemachineConfiner2D>();
+    }
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            confiner.BoundingShape2D = mapBoundry;
+            UpdatePlayerPosition(collision.gameObject);
+        }
+    }
+
+    private void UpdatePlayerPosition(GameObject player)
+    {
+        Vector3 newPos = player.transform.position;
+
+        switch (direction)
+        {
+            case Direction.Up:
+                newPos.y += additivePos;
+                break;
+            case Direction.Down:
+                newPos.y -= additivePos;
+                break;
+            case Direction.Left:
+                newPos.x += additivePos;
+                break;
+            case Direction.Right:
+                newPos.x -= additivePos;
+                break;
+        }
+
+        player.transform.position = newPos;
+    }
+}
+
+ 
--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -1,100 +1,100 @@
-using System.Collections;
-using TMPro;
-using UnityEngine;
-using UnityEngine.UI;
+//using System.Collections;
+//using TMPro;
+//using UnityEngine;
+//using UnityEngine.UI;
 
-public class NPC : MonoBehaviour, IInteractable
-{
-    public NPCDialogus DialogusData;
-    public GameObject DialoguePanel;
-    public TMP_Text DialogueText, nameText;
-    public Image PortraitImage;
+//public class NPC : MonoBehaviour, IInteractable
+//{
+//    public NPCDialogus DialogusData;
+//    public GameObject DialoguePanel;
+//    public TMP_Text DialogueText, nameText;
+//    public Image PortraitImage;
 
-    private int DialogueIndex;
-    private bool isTyping, isDialogueActive;
+//    private int DialogueIndex;
+//    private bool isTyping, isDialogueActive;
 
-    public bool CanInteract()
-    {
-        return !isDialogueActive;
-    }
+//    public bool CanInteract()
+//    {
+//        return !isDialogueActive;
+//    }
 
-    public void Interact()
-    {
-        if (dialogueData == null || (PauseController.IsGamePaused && !isDialogueActive))
-            return;
+//    public void Interact()
+//    {
+//        if (dialogueData == null || (PauseController.IsGamePaused && !isDialogueActive))
+//            return;
 
-        if (!isDialogueActive)
-        {
-            NextLine();
-        }
-        else
-        {
-            StartDialogue();
-        }
-    }
+//        if (!isDialogueActive)
+//        {
+//            NextLine();
+//        }
+//        else
+//        {
+//            StartDialogue();
+//        }
+//    }
 
-    void StartDialogue()
-    {
-        isDialogueActive = true;
-        DialogueIndex = 0;
+//    void StartDialogue()
+//    {
+//        isDialogueActive = true;
+//        DialogueIndex = 0;
 
-        nameText.SetText(dialogueData.name);
-        PortraitImage.sprite = dialogueData.npcPortrait;
+//        nameText.SetText(dialogueData.name);
+//        PortraitImage.sprite = dialogueData.npcPortrait;
 
-        DialoguePanel.SetActive(true);
-        PauseController.SetPause(true);
+//        DialoguePanel.SetActive(true);
+//        PauseController.SetPause(true);
 
-        StartCoroutine(TypeLine());
-    }
+//        StartCoroutine(TypeLine());
+//    }
 
-    void NextLine()
-    {
-        if (isTyping)
-        {
-            StopAllCoroutines();
-            DialogueText.SetText(dialogueData.dialogueLines[dialogueIndex]);
-            isTyping = false;
-        }
-        else if (++DialogueIndex < dialogueData.dialogueLines.Length)
-        {
-            //if
-            StartCoroutine(TypeLine());
-        }
-        else
-        {
-            EndDialogue();
-        }
-    }
+//    void NextLine()
+//    {
+//        if (isTyping)
+//        {
+//            StopAllCoroutines();
+//            DialogueText.SetText(dialogueData.dialogueLines[dialogueIndex]);
+//            isTyping = false;
+//        }
+//        else if (++DialogueIndex < dialogueData.dialogueLines.Length)
+//        {
+//            //if
+//            StartCoroutine(TypeLine());
+//        }
+//        else
+//        {
+//            EndDialogue();
+//        }
+//    }
 
-    IEnumerator TypeLine()
-    {
-        isTyping = true;
-        DialogueText.SetText("");
+//    IEnumerator TypeLine()
+//    {
+//        isTyping = true;
+//        DialogueText.SetText("");
 
-        foreach (char letter in dialogueData.dialogueLines[dialogueIndex])
-        {
-            DialogueText.text += letter;
-            yield return new WaitForSeconds(dialogueData.typingSpeed);
-        }
+//        foreach (char letter in dialogueData.dialogueLines[dialogueIndex])
+//        {
+//            DialogueText.text += letter;
+//            yield return new WaitForSeconds(dialogueData.typingSpeed);
+//        }
 
-        isTyping = false;
+//        isTyping = false;
 
-        if (dialogueData.autoProgressLines.Length > DialogueIndex && dialogueData.autoProgressLines[dialogueIndex])
-        {
-            yield return new WaitForSeconds(DialogueData.autoProgressDelay);
-            NextLine();
-        }
-    }
+//        if (dialogueData.autoProgressLines.Length > DialogueIndex && dialogueData.autoProgressLines[dialogueIndex])
+//        {
+//            yield return new WaitForSeconds(DialogueData.autoProgressDelay);
+//            NextLine();
+//        }
+//    }
 
-    public void EndDialogue()
-    {
-        StopAllCoroutines();
-        isDialogueActive = false;
-        DialogueText.SetText("");
-        DialoguePanel.SetActive(false);
-        PauseController.SetPause(false);
-    }
+//    public void EndDialogue()
+//    {
+//        StopAllCoroutines();
+//        isDialogueActive = false;
+//        DialogueText.SetText("");
+//        DialoguePanel.SetActive(false);
+//        PauseController.SetPause(false);
+//    }
 
-}
+//}
 
 

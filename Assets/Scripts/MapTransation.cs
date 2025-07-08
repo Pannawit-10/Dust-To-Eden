c0aaf5d2ca8e8033230add19c65368774b21438a
Pannawit-10 <52687@yupparaj.ac.th>
2025-07-08 07:45:34 +0700

    add

--- Assets/Scripts/MapTransation.cs
+++ Assets/Scripts/MapTransation.cs
@@ -1,58 +1,30 @@
 using Unity.Cinemachine;
-<<<<<<< HEAD
-using UnityEditor.Experimental.GraphView;
-using UnityEngine;
-
-public class MapTransation : MonoBehaviour
-{
-    [SerializeField] PolygonCollider2D mapBoundry;
-    CinemachineConfiner confiner;
-    [SerializeField] Direction direction;
-    [SerializeField] float additivePos - 2f;
-=======
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class MapTransation : MonoBehaviour
 {
-    [SerializeField] PolygonCollider2D mapBoundry;
-    CinemachineConfiner2D confiner;
+    [SerializeField] PolygonCollider2D mapBoundary;
+    CinemachineConfiner2D Confiner;
     [SerializeField] Direction direction;
     [SerializeField] float additivePos = 2f;
->>>>>>> 790013cae5a3a7b601ff9d733d9ceecd0a2493b5
 
     enum Direction { Up, Down, Left, Right }
 
     private void Awake()
     {
-<<<<<<< HEAD
-        confiner = FindObjectOfType<CinemachineConfiner>();
+        Confiner = Object.FindFirstObjectByType<CinemachineConfiner2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            confiner.m_BoundingShape2D = mapBoundry;
-        }
-    }
-}
-=======
-        confiner = Object.FindFirstObjectByType<CinemachineConfiner2D>();
-    }
-    
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            confiner.BoundingShape2D = mapBoundry;
+            confiner.m_BoundingShape2D = mapBoundary;
             UpdatePlayerPosition(collision.gameObject);
         }
     }
-
     private void UpdatePlayerPosition(GameObject player)
     {
         Vector3 newPos = player.transform.position;
@@ -76,6 +48,3 @@ public class MapTransation : MonoBehaviour
         player.transform.position = newPos;
     }
 }
-
- 
->>>>>>> 790013cae5a3a7b601ff9d733d9ceecd0a2493b5

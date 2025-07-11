f33f5a3b36775c0ba71af7a3d27e520c52d0c380
poommynynano <52936@yupparaj.ac.th>
2025-07-11 18:58:53 +0700

    add

--- Assets/InventoryController.cs
+++ Assets/InventoryController.cs
@@ -16,16 +16,16 @@ public class InventoryController : MonoBehaviour
     {
         itemDictionary = FindFirstObjectByType<ItemDictionary>();
 
-        //for (int i = 0; i < slotCount; i++)
-        //{
-        //    Slot slot = Instantiate(slotPrefab, inventoryPanel.transform).GetComponent<Slot>();
-        //    if (i < itemPrefabs.Length)
-        //    {
-        //        GameObject item = Instantiate(itemPrefabs[i], slot.transform);
-        //        item.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
-        //        slot.currentItem = item;
-        //    }
-        //}
+        for (int i = 0; i < slotCount; i++)
+        {
+            Slot slot = Instantiate(slotPrefab, inventoryPanel.transform).GetComponent<Slot>();
+            if (i < itemPrefabs.Length)
+            {
+                GameObject item = Instantiate(itemPrefabs[i], slot.transform);
+                item.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                slot.currentItem = item;
+            }
+        }
     }
 
     public List<InventorySaveData> GetInventoryItem()

926058254437cea6e83635139ec38c2bdf6dfa1f
poommynynano <52936@yupparaj.ac.th>
2025-07-11 18:53:48 +0700

    inventorycontroller50percentadd

--- Assets/ItemDictionary.cs
+++ Assets/ItemDictionary.cs
@@ -1,16 +1,37 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemDictionary : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public List<Item> itemPrefabs;
+    private Dictionary<int, GameObject> itemDictionary;
+
+    private void Awake()
     {
-        
+        itemDictionary = new Dictionary<int, GameObject>();
+
+        //AutoIncrementaIds
+        for (int i = 0; i < itemPrefabs.Count; i++)
+        {
+            if (itemPrefabs[i] != null)
+            {
+                itemPrefabs[i].ID = i + i;
+            }
+        }
+        foreach (Item item in itemPrefabs)
+        {
+            itemDictionary[item.ID] = item.gameObject;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject GetItemPrefab(int itemID)
     {
-        
+        itemDictionary.TryGetValue(itemID, out GameObject prefab);
+        if (prefab == null)
+        {
+            Debug.LogWarning("Item with ID "+ itemID +" not found in dictionary");
+        }
+        return prefab;
     }
 }
---
+++ Assets/Scripts/InventorySaveData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[System.Serializable]
+public class InventorySaveData : MonoBehaviour
+{
+    public int itemID;
+    public int slotIndex; //The index of the sllot where the item is placed within our inventory
+}

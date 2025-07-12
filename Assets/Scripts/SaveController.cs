81729966b3876d7db399651ba5cddc2f25f32e29
poommynynano <52936@yupparaj.ac.th>
2025-07-12 16:21:45 +0700

    add

--- Assets/Scripts/SaveController.cs
+++ Assets/Scripts/SaveController.cs
@@ -1,45 +1,47 @@
-using System.IO;
-using Unity.Cinemachine;
-using UnityEngine;
+//using System.IO;
+//using Unity.Cinemachine;
+//using UnityEngine;
 
-public class SaveController : MonoBehaviour
-{
-    private string saveLocation;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        //Define save location
-        saveLocation = Path.Combine(Application.persistentDataPath, "saveData.json");
-        inventoryController = FindFirstObjectByType<InventoryController>();
+//public class SaveController : MonoBehaviour
+//{
+//    private string saveLocation;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        //Define save location
+//        saveLocation = Path.Combine(Application.persistentDataPath, "saveData.json");
+//        //inventoryController = FindFirstObjectByType<InventoryController>();
         
-        LoadGame();
-    }
+//        LoadGame();
+//    }
 
-    public void SaveGame()
-    {
-        SaveData saveData = new SaveData
-        {
-            playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position,
-            mapBoundary = FindFirstObjectByType<CinemachineConfiner2D>().BoundingShape2D.name,
-            inventorySaveData = inventoryController.GetInventoryItem()
-        };
+//    public void SaveGame()
+//    {
+//        SaveData saveData = new SaveData
+//        {
+//            playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position,
+//            mapBoundary = FindFirstObjectByType<CinemachineConfiner2D>().BoundingShape2D.name,
+//            //inventorySaveData = inventoryController.GetInventoryItem()
+//        };
 
-        File.WriteAllText(saveLocation, JsonUtility.ToJson(saveData));
+//        File.WriteAllText(saveLocation, JsonUtility.ToJson(saveData));
+    
+//    }
 
-    }
-
-    public void LoadGame()
-    {
-        if (File.Exists(saveLocation))
-        {
-            SaveData saveData = JsonUtility.FromJson<SaveData>(File.ReadAllText(saveLocation));
-            GameObject.FindGameObjectWithTag("Player").transform.position = saveData.playerPosition;
-            FindFirstObjectByType<CinemachineConfiner2D>().BoundingShape2D = GameObject.Find(saveData.mapBoundary).GetComponent<PolygonCollider2D>();
-            InventoryController.SetInventoryItems(savdData.inventorySaveData);
-        }
-        else
-        {
-            SaveGame();
-        }
-    }
-}
+//    public void LoadGame()
+//    {
+//        if (File.Exists(saveLocation))
+//        {
+//            SaveData saveData = JsonUtility.FromJson<SaveData>(File.ReadAllText(saveLocation));
+            
+//            GameObject.FindGameObjectWithTag("Player").transform.position = saveData.playerPosition;
+            
+//            FindFirstObjectByType<CinemachineConfiner2D>().BoundingShape2D = GameObject.Find(saveData.mapBoundary).GetComponent<PolygonCollider2D>();
+//            //InventoryController.SetInventoryItems(savdData.inventorySaveData);
+//        }
+//        else
+//        {
+//            SaveGame();
+//        }
+//    }
+//}

6150926b4bb32c42d9539f562a6a3227b916b5cb
poommynynano <52936@yupparaj.ac.th>
2025-08-02 23:23:56 +0700

    addd

--- Assets/Collecteble.cs
+++ Assets/Collecteble.cs
@@ -1,19 +1,19 @@
-using UnityEngine;
+//using UnityEngine;
 
-public class Collecteble : MonoBehaviour
-{
-    //player walks into collecteble
-    //add collecteble to   player
-    //delete collecteble from the screen
+//public class Collecteble : MonoBehaviour
+//{
+//    //player walks into collecteble
+//    //add collecteble to   player
+//    //delete collecteble from the screen
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Player player = collision.GetComponent<Player>();
+//    private void OnTriggerEnter2D(Collider2D collision)
+//    {
+//        Player player = collision.GetComponent<Player>();
 
-        if(player)
-        {
-            player.numspinach_0++;
-            Destroy(this.gameObject);
-        }
-    }
-}
+//        if(player)
+//        {
+//            player.numspinach_0++;
+//            Destroy(this.gameObject);
+//        }
+//    }
+//}
